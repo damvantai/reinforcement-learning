4a78c356b0fce2890dd76fe8e6529eb3bd5735b9
damvantai <tai94bn@gmail.com>
2017-10-17 14:37:40 +0700

    continue tictoetac

--- reinforcement-learning-an-introduction/chapter01/TicTacToe.py
+++ reinforcement-learning-an-introduction/chapter01/TicTacToe.py
@@ -5,4 +5,96 @@ Created on Tue Oct 17 13:58:25 2017
 @author: damvantai
 """
 
-from __future 
+from __future__ import print_function
+import numpy as np
+import pickle
+
+BOARD_ROWS = 3
+BOARD_COLS = 3
+BOARD_SIZE = BOARD_ROWS * BOARD_COLS
+
+class State:
+    def __init__(self):
+        # the board is represented by a n*n array,
+        # 1 represents chessman of the player who moves first,
+        # -1 represents chessman of another player
+        # self.end = None
+    
+    # calculate the hash value for one state, it's unique
+    def getHash(self):
+        if self.hashVal is None:
+            self.hashVal = 0
+            for i in self.data.reshape(BOARD_ROWS * BOARD_COLS):
+                if i == -1:
+                    i = 2
+                self.hashVal = self.hashVal * 3 + i
+        return int(self.hashVal)
+
+    # determine whether a player has won the game, or it's a tie
+    def isEnd(self):
+        if self.end is not None:
+            return self.end
+        results = []
+
+        # check row
+        for i in range(0, BOARD_ROWS):
+            results.append(np.sum(self.data[i, :]))
+
+        # check columns
+        for i in range(0, BOARD_COLS):
+            results.append(np.sum(self.data[:, i]))
+
+        # check diagonals
+        results.append(0)
+        for i in range(0, BOARD_ROWS):
+            results[-1] += self.data[i, i]
+        results.append(0)
+        for i in range(0, BOARD_ROWS):
+            results[-1] += self.data[i, BOARD_ROWS - 1 -i]
+
+        for result in results:
+            if result == 3:
+                self.winner = 1
+                self.end = True
+                return self.end
+            if result == -3:
+                self.winner = -1
+                self.end = True
+                return self.end
+
+        # whether it's a tie
+        sum = np.sum(np.abs(self.data))
+        if sum == BOARD_ROWS * BOARD_COLS:
+            self.winner = 0
+            self.end = True
+            return self.end
+
+        # game is still going on
+        self.end = False
+        return self.end
+
+    # @symbol 1 or -1
+    # put chessman symbol in position (i, j)
+    def nextState(self, i, j, symbol):
+        newState = State()
+        newState.data = np.copy(self.data)
+        newState.data[i, j] = symbol
+        return newState
+
+    # print the board
+    def show(self):
+        for i in range(0, BOARD_ROWS):
+            print('--------------')
+            out = '| '
+            for j in range(0, BOARD_COLS):
+                if self.data[i, j] == 1:
+                    token = '*'
+                if self.data[i, j] == 0:
+                    token = '0'
+                if self.data[i, j] == -1:
+                    token = 'x'
+                out += token + ' | '
+            print(out)
+        print('--------------')
+            
+
